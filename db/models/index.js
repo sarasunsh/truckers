d087eca5570a6bb260aa54844ae917f8026dc51d
Sara Sunshine <sarasunsh@gmail.com>
2016-11-01 12:53:36 -0400

    Item scaffold

--- db/models/index.js
+++ db/models/index.js
@@ -4,6 +4,20 @@
 // so any other part of the application could call sequelize.model('User')
 // to get access to the User model.
 
-const User = require('./user')
+const User = require('./user');
+const Order = require('./order');
+const MenuItem = require('./menuItem');
 
-module.exports = {User}
+// THIS NEEDS TO BE REFINED
+
+//// which one of these two approaches is better?
+// Difference between HasOne and BelongsTo -- http://docs.sequelizejs.com/en/latest/docs/associations/
+Order.hasOne(User);
+Order.belongsTo(User);  // I believe this approach will put a UserID in the Order table
+
+Order.belongsTo(Truck);
+
+User.hasMany(Order);
+
+
+module.exports = {User, Order, MenuItem};
