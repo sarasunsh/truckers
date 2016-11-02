584896feacb1e5690b4909abbd90d0ee386cfef4
Sara Sunshine <sarasunsh@gmail.com>
2016-11-02 15:26:55 -0400

    combined menu routes into truck routes

--- server/trucks.js
+++ server/trucks.js
@@ -14,6 +14,18 @@ customTruckRoutes.param('truckID', function(req, res, next, truckID){
     .catch(next);
 });
 
+// If menu item ID is part of the request parameter, this will store the appropriate item info on the request
+customTruckRoutes.param('itemID', function(req, res, next, truckID){
+    MenuItem.findById(req.params.menuID)
+    .then(foundMenuItem => {
+        req.item = foundMenuItem;
+        next()
+    })
+    .catch(next);
+});
+
+/// GET REQUESTS -------------------------------------------------------------
+
 // Route for main page -- get all trucks
 customTruckRoutes.get('/', function(req, res, next) {
     FoodTruck.findAll()
@@ -21,35 +33,76 @@ customTruckRoutes.get('/', function(req, res, next) {
     .catch(next);
 });
 
+// Route to page for specific truck
+customTruckRoutes.get('/:truckID', function(req, res, next) {
+    res.json(req.truck);
+});
 
 // Route to menu page for specific truck
-customTruckRoutes.get('/:truckID', function(req, res, next) {
-    res.send(req.truck);  // TO DO: do we need to res.json the response or is there a parser somewhere?
+customTruckRoutes.get('/:truckID/menu', function(req, res, next) {
+    MenuItem.findAll({
+        where: {food_truck_id: req.truck.id }
+    })
+    .then(items => res.json(items))
+    .catch(next);
 });
+
+// Route to page for specific item at specific truck
+customTruckRoutes.get('/:truckID/menu/:itemID', function(req, res, next) {
+    res.json(req.item);
+});
+
+
+/// POST REQUESTS -------------------------------------------------------------
 
 // Route to create new truck
 customTruckRoutes.post('/', function(req, res, next) {
     FoodTruck.create(req.body)
-    .then(newTruck => res.status(201).send(newTruck))
+    .then(newTruck => res.json(newTruck))
     .catch(next);
 });
+
+// Route to create new menu item for specific truck
+customTruckRoutes.post('/:truckID/menu', function(req, res, next) {
+    req.truck.createMenuItem(req.body) // TO DO: confirm that this works as expected
+    .then(newItem => res.json(newItem))
+    .catch(next);
+});
+
+
+/// PUT REQUESTS -------------------------------------------------------------
 
 // Route to update info on specific truck
 customTruckRoutes.put('/:truckID', function(req, res, next) {
     req.truck.update(req.body)
-    .then(updatedTruck => res.send(updatedTruck))
+    .then(updatedTruck => res.json(updatedTruck))
     .catch(next);
 });
 
-// Route to delete specific truck
-customTruckRoutes.delete('/:truckID', function(req, res, next) {
-    // TO DO: can we use req.truck for deleting? Will this automatically destroy all MenuItems
-    req.truck.destroy()
-    .then(destroyedTruck => res.send(destroyedTruck))
-    .catch(next)
+// Route to update info on menu item for specific truck
+customTruckRoutes.post('/:truckID/menu/:itemID', function(req, res, next) {
+    req.item.update(req.body)
+    .then(updatedItem => res.json(updatedItem))
+    .catch(next);
 });
 
 
+/// DELETE REQUESTS -------------------------------------------------------------
+
+// Route to delete specific truck
+customTruckRoutes.delete('/:truckID', function(req, res, next) {
+    // TO DO: can we use req.truck for deleting? Do we want to automatically destroy all MenuItems?
+    req.truck.destroy()
+    .then(destroyedTruck => res.json(destroyedTruck))
+    .catch(next)
+});
+
+// Route to delete specific truck
+customTruckRoutes.delete('/:truckID/menu/:itemID', function(req, res, next) {
+    req.item.destroy()
+    .then(destroyedItem => res.json(destroyedItem))
+    .catch(next)
+});
 
 
 module.exports = customTruckRoutes
