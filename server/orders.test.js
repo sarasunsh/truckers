6ebbdb82e9452010cf70aabdc0dc21cccd257248
mkewls <mw@mikewill.net>
2016-11-03 12:28:39 -0400

    fixed errors in orders tests

--- server/orders.test.js
+++ server/orders.test.js
@@ -3,21 +3,23 @@
 const request = require('supertest-as-promised')
 const {expect} = require('chai')
 const db = require('APP/db')
-const Orders = require('./orders')
 const app = require('./start')
+
+// Note that our tests require our 'Order Creation' POST route to work,
+// as the fakeOrder below used to create the test data is seeded through
+// that route
+const fakeOrder = {
+  id: 101,
+  userId: 110,
+  truckId: 101,
+  orders: {
+    menuItem: 'tacos',
+    quantity: 2
+  }
+}
 
 describe('api/orders', () => {
   before('wait for the db', () => db.didSync)
-
-  const fakeOrder = {
-          id: 100,
-          userId: 110,
-          truckId: 1,
-          orders: {
-            menuItem: 'tacos',
-            quantity: 2
-          }
-        }
 
   describe('creates an order', () => {
     it('POST to / creates an order with userId, truckId, and menu selections', () =>
@@ -36,92 +38,77 @@ describe('api/orders', () => {
         .expect(500)
       )
 
-  describe('gets all orders (admin)', () => {
+  describe('gets all orders (admin) and specific orders by id', () => {
 
     before(() =>
       request(app)
-        .post('/api/orders')
+        .post('api/orders')
         .send(fakeOrder)
+        .then(ok => console.log('fake data seeded for the following GET tests'))
     )
 
     it('GET to / gets all orders', () =>
       request(app)
         .get('/api/orders')
-        .then(res => expect(res.body).to.contain.({
+        .then(res => expect(res.body).to.contain({
           userId: 110
         }))
       )
-  }
 
-  describe('gets all orders for a given order id', () => {
-    before(() =>
-      Orders.create(fakeOrder)
-            .then()
-            .catch(() => console.log('Order.create not working'))
-    )
-
-    it('GET to :orderId gets a single order by id', () =>
+    it('GET to /:orderId gets a single order by id', () =>
       request(app)
-        .get('/api/orders/100')
-        .then(res => expect(res.body).to.contain.({
+        .get('/api/orders/101')
+        .then(res => expect(res.body).to.contain({
           userId: 110
         }))
     )
 
-    it('GET to :orderId returns an empty array if not found', () =>
+    it('GET to /:orderId returns an empty array if not found', () =>
       request(app)
         .get('/api/orders/101')
-        .then(res => expect(res.body).to.contain.({
+        .then(res => expect(res.body).to.contain({
           []
         }))
       )
   })
 
-  describe('gets all orders for a given user id', () => {
+  describe('gets all orders for a given user id or food truck', () => {
 
     before(() =>
-      Orders.create(fakeOrder)
-            .then()
-            .catch(() => console.log('Order.create not working'))
+      request(app)
+        .post('api/orders')
+        .send(fakeOrder)
+        .then(ok => console.log('fake data seeded for the following GET tests'))
     )
 
     it('GET to /user/:userId gets all orders by a user', () =>
       request(app)
-        .get('/api/orders/110')
-        .then(res => expect(res.body).to.contain.({
-          truckId: 1
+        .get('/api/orders/user/110')
+        .then(res => expect(res.body).to.contain({
+          truckId: 101
         }))
       )
 
     it('GET to /user/:userId returns an empty array if none found', () =>
       request(app)
-        .get('/api/orders/110')
-        .then(res => expect(res.body).to.contain.({
+        .get('/api/orders/user/151')
+        .then(res => expect(res.body).to.contain({
           []
         }))
       )
-  })
 
-  describe('gets all orders for a given food truck', () => {
-
-    before(() =>
-      Orders.create(fakeOrder)
-            .then()
-            .catch(() => console.log('Order.create not working'))
-    )
-
-    it('GET to /orders/:truckid can find orders by truck id', () =>
+    it('GET to /truck/:truckid can find orders by truck id', () =>
       request(app)
-        .get('/api/orders/1')
-        .then(res => expect(res.body).to.contain.({
+        .get('/api/orders/truck/101')
+        .then(res => expect(res.body).to.contain({
           userId: 110
         }))
       )
 
-    it('GET to /orders/:truckid returns an empty array if a truck has no orders', () =>
+    it('GET to /truck/:truckid returns an empty array if a truck has no orders', () =>
       request(app)
-        .get('/api/orders/1')
-        .then(res => expect(res.body).to.contain.({
+        .get('/api/orders/truck/151')
+        .then(res => expect(res.body).to.contain({
           []
         }))
       )
@@ -130,14 +117,15 @@ describe('api/orders', () => {
   describe('allows a vendor to modify or delete orders', () => {
 
     before(() =>
-      Orders.create(fakeOrder)
-            .then()
-            .catch(() => console.log('Order.create not working'))
+      request(app)
+        .post('api/orders')
+        .send(fakeOrder)
+        .then(ok => console.log('fake data seeded for the following PUT/DELETE tests'))
     )
 
     it('PUT to /orders/:orderId updates an order', () =>
       request(app)
-        .put('/api/orders/100')
+        .put('/api/orders/101')
         .send({
           orders: {
             menuItem: 'enchiladas',
@@ -146,8 +134,8 @@ describe('api/orders', () => {
         })
         .then(res => {
           return request(app)
-            .get('/api/orders/100')
-            .then(res => expect(res.body).to.contain.({
+            .get('/api/orders')
+            .then(res => expect(res.body).to.contain({
               orders: {
                 menuItem: 'enchiladas',
                 quantity: 1
@@ -158,14 +146,17 @@ describe('api/orders', () => {
 
     it('DELETE to orders/:orderId deletes an order', () =>
       request(app)
-        .delete('/api/orders/100')
+        .delete('/api/orders/101')
         .then(res => {
           return request(app)
-            .get('/api/orders/100')
-            .then(res => expect(res.body).to.contain.({
-              []
+            .get('/api/orders')
+            .then(res => expect(res.body).to.not.contain({
+              orders: {
+                menuItem: 'tacos',
+                quantity: 2
+              }
             }))
       })
     )
-    
+
 })
