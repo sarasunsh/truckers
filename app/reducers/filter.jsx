a32c6a9fe3df57fc779db3b4009942479ca65d48
mkewls <mw@mikewill.net>
2016-11-04 22:04:31 -0400

    modified filter reducer and action-creator to improve filtering capabilities

--- app/reducers/filter.jsx
+++ app/reducers/filter.jsx
@@ -19,25 +19,43 @@ export const addFilterAction = (str) => ({
 })
 
 export const showFilteredTrucksAction = () => ({
-    type: FILTER_TRUCKS,
-    trucks
+    type: FILTER_TRUCKS
 })
 
 // -=-=-=-=-= REDUCER =-=-=-=-=-=-
 
+// check whether a truck matches our filters
+const checkFilters(truck, filters) => {
+  let truckProps = [];
+  if (truck.isOpen) { truckProps.push('open')};  // filter is 'open'
+  if (truck.rating >= 4) { truckProps.push('highRated') } // filter is 'highRated'
+  // now we manually add props to avoid unintentional matches with non-filter prop values
+  truckProps = [...truckProps, truck['cuisine'], truck['avgPrice']];
+  return (truckProps.some(truckProp => {        // iterate over filterable props
+    return filters.indexOf(truckProp) >= 0      // return true if prop hits a current filter
+  }))
+}
+
+// reduce our filter actions
 export default filterReducer = (state = store.getState(), action) => {
     switch(action.type){
         case REMOVE_FILTER: {
-            let idx = state.filters.indexOf(action.filter);
-            state.filters.splice(idx, 1);
-            return state.filters;
+            let idx = state.filters.indexOf(action.filter);  // find index of filter to remove
+            state.filters.splice(idx, 1);                    // splice it out
+            return state.filters;                            // return the modified filters array
         }
         case ADD_FILTER: {
-            return state.filters.push(action.filter);
+            return ([...state.filters, action.filter]);      // concat new filter
         }
         case FILTER_TRUCKS : {
-            return state.trucks;
+            return state.trucks.map(truck => {               // iterate over filters
+              if (checkFilters(truck, state.filters)) {      // check if truck matches a filter
+                truck.display = 'block';                     // if yes, show
+              } else {
+                truck.display = 'none';                      // if not, hide
+              }
+            })
         }
-        default: return state
+        default: return state;
     }
 }
